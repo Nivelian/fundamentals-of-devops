fa8f29c053fb7c8781611661aaff37ced51c3bbe
Alex Mazur <a.mazur@custimy.io>
2025-08-10 11:57:27 +0200

    Add changes

--- ch5/tofu/live/ci-cd-permissions/main.tf
+++ ch5/tofu/live/ci-cd-permissions/main.tf
@@ -19,14 +19,13 @@ module "iam_roles" {
 
   enable_iam_role_for_testing = true                            
 
-  # TODO: fill in your own repo name here!
   github_repo      = "Nivelian/fundamentals-of-devops" 
   lambda_base_name = "lambda-sample"                            
 
-  # enable_iam_role_for_plan  = true                                
-  # enable_iam_role_for_apply = true                                
+  enable_iam_role_for_plan  = true                                
+  enable_iam_role_for_apply = true                                
 
-  # # TODO: fill in your own bucket and table name here!
-  # tofu_state_bucket         = "fundamentals-of-devops-tofu-state" 
-  # tofu_state_dynamodb_table = "fundamentals-of-devops-tofu-state" 
+
+  tofu_state_bucket         = "alex-fundamentals-of-devops-tofu-state" 
+  tofu_state_dynamodb_table = "alex-fundamentals-of-devops-tofu-state" 
 }
--- ch5/tofu/live/tofu-state/main.tf
+++ ch5/tofu/live/tofu-state/main.tf
@@ -6,6 +6,5 @@ module "state" {
   source  = "brikis98/devops/book//modules/state-bucket"
   version = "1.0.0"
 
-  # TODO: fill in your own bucket name!
-  name = "fundamentals-of-devops-tofu-state"
+  name = "alex-fundamentals-of-devops-tofu-state"
 }
